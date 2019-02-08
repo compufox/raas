830ae9a504c1f00743d7dbaa0bd364c4f1394e44
Zac <thezacattacks@gmail.com>
2019-02-07 22:46:40 -0500

    updated errors
    commented out puts statement (thanks Ellie~)

--- maas.rb
+++ maas.rb
@@ -1,12 +1,15 @@
 require 'cuba'
 require 'json'
 
-ApiError = { error: 'merp' }
+ApiErrors = [ { error: 'merp' },
+              { error: 'OvO'  },
+              { error: 'UvU'  }
+            ]
 
 Cuba.define do
   on get do
     on 'api/v1/merp' do
-      puts 'gotem'
+      # puts 'gotem'
       res.headers['Content-Type'] = 'application/json; charset=utf-8'
       res.write "merp".to_json
     end
@@ -24,7 +27,7 @@ Cuba.define do
 
       on true do
         res.headers['Content-Type'] = 'application/json; charset=utf-8'
-        res.write ApiError.to_json
+        res.write ApiErrors.sample.to_json
       end
     end
   end
